60c4a30667925073e51d23fbb687422cac92cdad
TubiTiffany <tubi.tiffany@gmail.com>
2020-05-03 10:48:34 -0400

    Updates

---
+++ precache-manifest.a801e94caff05c0f580d5ba749fd6625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3493d2301987c24b63ea0c54398bf69",
+    "url": "/tubitiffany/tiffany-tse-project-five/index.html"
+  },
+  {
+    "revision": "b43efeb012dc45d404d4",
+    "url": "/tubitiffany/tiffany-tse-project-five/static/css/main.167f4ffe.chunk.css"
+  },
+  {
+    "revision": "04befb5561eef4cb7650",
+    "url": "/tubitiffany/tiffany-tse-project-five/static/js/2.729b9354.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tubitiffany/tiffany-tse-project-five/static/js/2.729b9354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43efeb012dc45d404d4",
+    "url": "/tubitiffany/tiffany-tse-project-five/static/js/main.e1ef2800.chunk.js"
+  },
+  {
+    "revision": "39d5d09f0f07cfa04451",
+    "url": "/tubitiffany/tiffany-tse-project-five/static/js/runtime-main.2a7aa7ec.js"
+  }
+]);
